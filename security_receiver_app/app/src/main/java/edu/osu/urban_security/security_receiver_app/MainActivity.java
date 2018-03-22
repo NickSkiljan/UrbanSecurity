d0e0ce0c76b6e835e51f13b9ca5668ec92d308a5
Nick Skiljan <nickskiljan@gmail.com>
2018-03-22 15:04:41 -0400

    App shows all names from users

--- security_receiver_app/app/src/main/java/edu/osu/urban_security/security_receiver_app/MainActivity.java
+++ security_receiver_app/app/src/main/java/edu/osu/urban_security/security_receiver_app/MainActivity.java
@@ -2,21 +2,29 @@ package edu.osu.urban_security.security_receiver_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // [START declare_database_ref]
+    private static final String TAG = MainActivity.class.getSimpleName();
     private DatabaseReference mDatabase;
     private DatabaseReference mSOS;
+    private String userId;
+    private ArrayList<String> SOSs = new ArrayList<>();
     // [END declare_database_ref]
 
     @Override
@@ -24,21 +32,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // [START initialize_database_ref]
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        // [END initialize_database_ref]
-
         mSOS = mDatabase.child("users");
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+
+                        //Update every second
+                        Thread.sleep(1000);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateList();
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+
+        };
+        t.start();
+
+    }
+
+    public void updateList() {
+
+        mSOS.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+                    SOSs.add(user.name);
+                    System.out.println(user.name);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Error: " + databaseError);
+            }
+        });
+
+
 
         LinearLayout linearLayout = new LinearLayout(this);
         ListView SOSListView = new ListView(this);
         SOSListView.setPadding(20, 300, 20, 100);
-
-        ArrayList<String> SOSs = new ArrayList<>();
-
-        SOSs.add("Nick Skiljan");
-        SOSs.add("Cloudy Patel");
-        SOSs.add("Joe Simons");
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, SOSs);
         SOSListView.setAdapter(adapter);
@@ -46,9 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
         linearLayout.addView(SOSListView);
         this.setContentView(linearLayout, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-
-
-
-
     }
+
+
 }
---
+++ security_receiver_app/app/src/main/java/edu/osu/urban_security/security_receiver_app/User.java
@@ -0,0 +1,36 @@
+package edu.osu.urban_security.security_receiver_app;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Ravi Tamada on 07/10/16.
+ * www.androidhive.info
+ */
+
+@IgnoreExtraProperties
+public class User {
+
+    public String name;
+    public String latitude;
+    public String longitude;
+    public String altitude;
+    public boolean moving;
+    public String phone_number;
+    public String address;
+
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String name, String latitude, String longitude, String altitude, boolean moving, String phone_number, String address) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
+        this.moving = moving;
+        this.phone_number = phone_number;
+        this.address = address;
+    }
+}

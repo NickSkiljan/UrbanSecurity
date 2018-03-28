5e0c391c263247f6d39168725f8ec3acd66342e0
Nick Skiljan <nickskiljan@gmail.com>
2018-03-28 13:53:49 -0400

    Added decryption code (commented out until AES file is copiedd over)

--- security_receiver_app/app/src/main/java/edu/osu/urban_security/security_receiver_app/User.java
+++ security_receiver_app/app/src/main/java/edu/osu/urban_security/security_receiver_app/User.java
@@ -11,7 +11,6 @@ public class User {
     public String latitude;
     public String longitude;
     public String altitude;
-    public boolean moving;
     public String phone_number;
     public String address;
     public byte[] privateKey;
@@ -24,14 +23,40 @@ public class User {
     public User() {
     }
 
-    public User(String name, String latitude, String longitude, String altitude, boolean moving, String phone_number, String address, byte[] privateKey) {
+    public User(String name, String latitude, String longitude, String altitude, String phone_number, String address, byte[] privateKey) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
         this.altitude = altitude;
-        this.moving = moving;
         this.phone_number = phone_number;
         this.address = address;
         this.privateKey = privateKey;
+
+        this.decrypt(privateKey);
+    }
+
+
+    public void decrypt(byte[] privateKey) {
+
+        /*AES obj = new AES();
+
+        byte[] decryptedName = obj.decrypt(privateKey, this.name);
+        this.name = new String(decryptedName);
+
+        byte[] decryptedLatitude = obj.decrypt(privateKey, this.latitude);
+        this.latitude = new String(decryptedLatitude);
+
+        byte[] decryptedLongitude = obj.decrypt(privateKey, this.longitude);
+        this.longitude = new String(decryptedLongitude);
+
+        byte[] decryptedAltitude = obj.decrypt(privateKey, this.altitude);
+        this.altitude = new String(decryptedAltitude);
+
+        byte[] decryptedPhoneNumber = obj.decrypt(privateKey, this.phone_number);
+        this.phone_number = new String(decryptedPhoneNumber);
+
+        byte[] decryptedAddress = obj.decrypt(privateKey, this.address);
+        this.address = new String(decryptedAddress);*/
+
     }
 }

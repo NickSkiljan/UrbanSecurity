78501f5af2aabd4fa80fa1c27ab092bd49673898
powell759 <maxrpowell@gmail.com>
2018-03-28 15:15:50 -0400

    Add empty encryption and SOS methods to Global

--- app/app/src/main/java/edu/osu/urban_security/security_app/models/Globals.java
+++ app/app/src/main/java/edu/osu/urban_security/security_app/models/Globals.java
@@ -1,5 +1,7 @@
 package edu.osu.urban_security.security_app.models;
 
+import android.util.Log;
+
 /**
  * Created by sunnypatel on 3/27/18.
  */
@@ -8,6 +10,7 @@ public class Globals {
     private static Globals instance;
 
     public User user;
+    private static final String TAG = "GLOBALS";
 
     /**
      * Singleton instance instantiation
@@ -18,6 +21,21 @@ public class Globals {
             instance=new Globals();
         }
         return instance;
+    }
+
+    /*
+     * TODO: Encryption should happen here
+     */
+    private String encryptString(String input){
+        String output = input;
+        return output;
+    }
+    /**
+     * Pushes time stamp to sos
+     * Updates user location
+     */
+    public void pushSOS(){
+        Log.d(TAG, "pushing SOS");
     }
 
     /**
